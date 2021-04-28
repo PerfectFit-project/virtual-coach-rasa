08b09a72f1c834ceaab99b5ace5251a63372d3f7
Robin Richardson <raarichardson@gmail.com>
2021-04-28 14:42:02 +0200

    Add error handling for case in which non-ODS is in response

--- virtual-coach-server/psycho_education.py
+++ virtual-coach-server/psycho_education.py
@@ -9,13 +9,19 @@ from config import PSYCHO_EDUCATION_URL, TEMP_PSYCHO_EDUCATION_ODS_LOCATION
 def fetch_random_psycho_education():
     r = requests.get(PSYCHO_EDUCATION_URL)
     if r.status_code != 200:
-        logging.error(f'Failed to fetch CSV from URL: {PSYCHO_EDUCATION_URL} - received response {r.status_code}.')
+        logging.error(f'Failed to fetch from URL: {PSYCHO_EDUCATION_URL} - received response {r.status_code}.')
         return None
 
-    # Convert response body to pandas dataframe
+    # Write response body to local file
     with open(TEMP_PSYCHO_EDUCATION_ODS_LOCATION, 'wb') as outfile:
         outfile.write(r.content)
-    df = read_ods(TEMP_PSYCHO_EDUCATION_ODS_LOCATION, sheet=1)
+
+    # Attempt to read ODS into pandas dataframe
+    try:
+        df = read_ods(TEMP_PSYCHO_EDUCATION_ODS_LOCATION, sheet=1)
+    except KeyError as ke:
+        logging.error(f'Response from {PSYCHO_EDUCATION_URL} does not look like an ODS file: KeyError {ke}.')
+        return None
 
     # Choose a row at random, and return it as a dict
     random_row = df.sample().to_dict("records")[0]

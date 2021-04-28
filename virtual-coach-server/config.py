ead42e0250444dd15ece4a4061ec1a39e7b54212
Robin Richardson <raarichardson@gmail.com>
2021-04-28 14:35:53 +0200

    Fetch and read ODS format instead

--- virtual-coach-server/config.py
+++ virtual-coach-server/config.py
@@ -1,1 +1,2 @@
-PSYCHO_EDUCATION_URL = 'https://surfdrive.surf.nl/files/index.php/s/CoqsuoGRDffhtnj/download'
+PSYCHO_EDUCATION_URL = 'https://surfdrive.surf.nl/files/index.php/s/hOPCmR9AFQgDALd/download'
+TEMP_PSYCHO_EDUCATION_ODS_LOCATION = '/tmp/psych_ed.ods'
--- virtual-coach-server/psycho_education.py
+++ virtual-coach-server/psycho_education.py
@@ -1,10 +1,9 @@
 import requests
-import csv
 from io import StringIO
-import random
+from pandas_ods_reader import read_ods
 import logging
 
-from config import PSYCHO_EDUCATION_URL
+from config import PSYCHO_EDUCATION_URL, TEMP_PSYCHO_EDUCATION_ODS_LOCATION
 
 
 def fetch_random_psycho_education():
@@ -13,13 +12,14 @@ def fetch_random_psycho_education():
         logging.error(f'Failed to fetch CSV from URL: {PSYCHO_EDUCATION_URL} - received response {r.status_code}.')
         return None
 
-    # Read CSV from response body and return random row
-    reader = csv.DictReader(StringIO(r.text))
-    csvrows = list(reader)
-    if 'Formulation Email' not in csvrows[0]:
-        logging.error(f'Response from URL {PSYCHO_EDUCATION_URL} was not valid csv. No "Formulation Email" column.')
-        return None
-    return random.choice(csvrows)
+    # Convert response body to pandas dataframe
+    with open(TEMP_PSYCHO_EDUCATION_ODS_LOCATION, 'wb') as outfile:
+        outfile.write(r.content)
+    df = read_ods(TEMP_PSYCHO_EDUCATION_ODS_LOCATION, sheet=1)
+
+    # Choose a row at random, and return it as a dict
+    random_row = df.sample().to_dict("records")[0]
+    return random_row
 
 
 if __name__ == "__main__":

0d189009c065346d2ac7de1e1d4fc175e5c33209
Sven van der Burg <s.vanderburg@esciencecenter.nl>
2021-05-11 09:18:04 +0200

    Make RASA_AGENT_URL into a constant

--- niceday-broker/index.js
+++ niceday-broker/index.js
@@ -10,14 +10,14 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 require('dotenv').config()
 const THERAPIST_USER_ID = parseInt(process.env.THERAPIST_USER_ID)
 const TOKEN = process.env.NICEDAY_TOKEN
+const RASA_AGENT_URL = process.env.RASA_AGENT_URL
 
 /**
  * Request a response from rasa for a given text message
  **/
 function requestRasa(text, userId, callback) {
     var xhr = new XMLHttpRequest();
-    var url = 'http://localhost:5005/webhooks/rest/webhook';
-    xhr.open("POST", url, true);
+    xhr.open("POST", RASA_AGENT_URL, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = callback
     var data = JSON.stringify({"sender": userId, "message": text});

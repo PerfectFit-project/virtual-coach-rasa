47d609d2d4d008fd0e77fd32d333ef4f00ca5aa0
Robin Richardson <raarichardson@gmail.com>
2021-08-18 16:27:44 +0200

    Add comment for the Profiles fetch code

--- niceday-api/service/ProfilesService.js
+++ niceday-api/service/ProfilesService.js
@@ -10,6 +10,10 @@ require('dotenv').config();
 const THERAPIST_USER_ID = parseInt(process.env.THERAPIST_USER_ID, 10);
 const TOKEN = process.env.NICEDAY_TOKEN;
 
+/**
+ * Fetch the user profile corresponding to the given user ID.
+ * Returns the profile as JSON.
+ * */
 exports.getUserProfile = function (userId) {
   return new Promise((resolve, reject) => {
     userNetwork

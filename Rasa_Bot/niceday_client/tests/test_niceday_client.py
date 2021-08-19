f8a0608e3070af7187dce6e6873f7fd8981c7ba0
Robin Richardson <raarichardson@gmail.com>
2021-08-19 13:30:47 +0200

    Add simple test for get_profile()

--- Rasa_Bot/niceday_client/tests/test_niceday_client.py
+++ Rasa_Bot/niceday_client/tests/test_niceday_client.py
@@ -1,6 +1,9 @@
 from niceday_client import NicedayClient
 
-client = NicedayClient()
-
-profile = client.get_profile(38527)
-print(profile)
+def test_get_profile():
+    client = NicedayClient()
+    existing_user_id = 38527
+    profile = client.get_profile(existing_user_id)
+    assert isinstance(profile, dict)
+    assert 'id' in profile
+    assert 'userProfile' in profile

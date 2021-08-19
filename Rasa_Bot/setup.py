972c187a8f5f97392f38ca2de531f08adbf18923
Nele Albers <nalbers1@tudelft.net>
2021-08-19 20:13:35 +0200

    setup.py add setup

--- Rasa_Bot/setup.py
+++ Rasa_Bot/setup.py
@@ -1,3 +1,8 @@
 import os
+from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+      name = "rasa_bot", classifiers = [],
+    packages=find_packages(where="demo"))

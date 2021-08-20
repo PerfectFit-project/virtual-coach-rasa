d4a33e5f6b72f871d8b67d090d72c92f30f803f6
Nele Albers <nalbers1@tudelft.net>
2021-08-20 08:29:14 +0200

    addressed warning on unknown mark pytest

--- Rasa_Bot/tests/test_actions.py
+++ Rasa_Bot/tests/test_actions.py
@@ -16,13 +16,12 @@ sys.path.append(parentdir)
 from actions import actions
 
 # Update once the function tested here works correctly
-@pytest.mark.asyncio
-async def test_run_action_save_plan_week_calendar( 
+def test_run_action_save_plan_week_calendar( 
     dispatcher: CollectingDispatcher,
     domain: DomainDict):
     tracker = WEEKLY_PLAN_TRACKER
     action = actions.SavePlanWeekCalendar()
-    events = await action.run(dispatcher, tracker, domain)
+    events = action.run(dispatcher, tracker, domain)
     expected_events = [
         SlotSet("success_save_calendar_plan_week", True),
     ]

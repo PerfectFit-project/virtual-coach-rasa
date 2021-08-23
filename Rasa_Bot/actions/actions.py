9845ff7e461730f7e1fc819d5ee7e2a27ea03970
Cunliang Geng <c.geng@esciencecenter.nl>
2021-08-23 16:35:46 +0200

    Add Celery scheduler and RASA reminder actions

--- Rasa_Bot/actions/actions.py
+++ Rasa_Bot/actions/actions.py
@@ -3,9 +3,12 @@
 #
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
+import datetime
+
 from paalgorithms import weekly_kilometers
 from rasa_sdk import Action
-from rasa_sdk.events import SlotSet
+from rasa_sdk.events import SlotSet, ReminderScheduled
+
 
 AGE = 30  # We should get this value from a database.
 
@@ -36,9 +39,8 @@ class GetPlanWeek(Action):
 
         return [SlotSet("plan_week", plan)]
 
+
 # Save weekly plan in calendar
-
-
 class SavePlanWeekCalendar(Action):
     def name(self):
         return "action_save_plan_week_calendar"
@@ -48,3 +50,40 @@ class SavePlanWeekCalendar(Action):
         success = True
 
         return [SlotSet("success_save_calendar_plan_week", success)]
+
+
+# Set reminder, triggered by external scheduler
+class ActionSetReminder(Action):
+    """To schedule a reminder"""
+
+    def name(self):
+        return "action_set_reminder"
+
+    async def run(self, dispatcher, tracker, domain):
+
+        date0 = datetime.datetime.now()
+
+        # used only for development
+        t = 2
+        dispatcher.utter_message(f"I will remind you in {t} seconds.")
+        date = date0 + datetime.timedelta(seconds=t)
+
+        # the daily reminder
+        # TODO: get user's time setting from database.
+        # Here using a fixed time 9:00AM.
+        # dispatcher.utter_message("I will remind you 9:00AM every day.")
+        # reminder_hour = 9
+        # date = datetime.datetime(date0.year, date0.month, date0.day,
+        #                         reminder_hour, 0, 0, 0)
+        # if (date0.hour > reminder_hour):
+        #     date = date + datetime.timedelta(days=1)
+
+        reminder = ReminderScheduled(
+            "EXTERNAL_utter_reminder",
+            trigger_date_time=date,
+            entities="",
+            name="Daily Reminder",
+            kill_on_user_message=False,
+        )
+
+        return [reminder]
---
+++ rasa_scheduler/rasa_scheduler/celery.py
@@ -0,0 +1,23 @@
+import os
+import requests
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rasa_scheduler.settings')
+
+app = Celery('rasa_scheduler')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def trigger_rasa_reminder(self):
+    """Task to trigger RASA to set reminder for every user.
+    """
+    # TODO: get user IDs from database
+    # TODO: add Celery error handling
+    user_ids = ['Kees', ]
+    for user in user_ids:
+        endpoint = f'http://localhost:5005/conversations/{user}/trigger_intent'
+        headers = {'Content-Type': 'application/json'}
+        params = {'output_channel': 'latest'}
+        data = '{"name": "EXTERNAL_set_reminder"}'
+        requests.post(endpoint, headers=headers, params=params, data=data)

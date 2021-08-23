eeb1e8006637e5ec5dafecf3ef86c7319bbae1b2
Cunliang Geng <c.geng@esciencecenter.nl>
2021-08-23 17:25:23 +0200

    Update rasa with reminder action

--- Rasa_Bot/actions/actions.py
+++ Rasa_Bot/actions/actions.py
@@ -3,9 +3,14 @@
 #
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
+import datetime
+from typing import Any, Text, Dict, List
+
 from paalgorithms import weekly_kilometers
-from rasa_sdk import Action
-from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet, ReminderScheduled, ReminderCancelled
+from rasa_sdk.executor import CollectingDispatcher
+
 
 AGE = 30  # We should get this value from a database.
 
@@ -48,3 +53,62 @@ class SavePlanWeekCalendar(Action):
         success = True
 
         return [SlotSet("success_save_calendar_plan_week", success)]
+
+
+# Set auto reminder, triggered by external scheduler
+class ActionSetReminder(Action):
+    """To schedule a reminder"""
+
+    def name(self) -> Text:
+        return "action_set_reminder"
+
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        # TODO: read user's time setting
+        date0 = datetime.datetime.now()
+        # reminder_hour = 9
+        # date = datetime.datetime(
+        #     date0.year,
+        #     date0.month,
+        #     date0.day,
+        #     reminder_hour,
+        #     0,
+        #     0,
+        #     0)
+        # if (date0.hour > reminder_hour):
+        #     date = date + datetime.timedelta(days=1)
+        date = date0 + datetime.timedelta(seconds=3)
+
+        reminder = ReminderScheduled(
+            "EXTERNAL_send_reminder",
+            trigger_date_time=date,
+            entities="",
+            name="Daily Reminder",
+            kill_on_user_message=False,
+        )
+
+        return [reminder]
+
+
+class ActionReactToReminder(Action):
+    """Reach out to user to remind the high risk situations."""
+
+    def name(self) -> Text:
+        return "action_react_to_reminder"
+
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(
+            f"Do you foresee any high risk situations for smoking today?")
+
+        return []

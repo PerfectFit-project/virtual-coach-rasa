8473f472633482b51811c8ef6f2ccea0014667e5
Robin Richardson <raarichardson@gmail.com>
2021-08-25 18:08:04 +0200

    fix pep8

--- onboarding/onboarding.py
+++ onboarding/onboarding.py
@@ -6,10 +6,11 @@ from sqlalchemy.orm import sessionmaker
 import sys
 
 sys.path.insert(0, "../niceday_client/")
-from niceday_client import NicedayClient
+from niceday_client import NicedayClient # noqa
 
 sys.path.insert(0, "../db/dbschema/")
-from models import Base, Users
+from models import Base, Users # noqa
+
 
 def onboard_user(userid):
     client = NicedayClient()
@@ -18,7 +19,7 @@ def onboard_user(userid):
     profile = client.get_profile(userid)
     print('Profile:', profile)
 
-    engine = create_engine('postgresql://root:root@localhost:5432/perfectfit', echo=False)
+    engine = create_engine('postgresql://root:root@localhost:5432/perfectfit')
     meta = MetaData()
     meta.reflect(bind=engine)
     session_maker = sessionmaker(bind=engine)
@@ -26,18 +27,20 @@ def onboard_user(userid):
 
     # Check if this user already exists in the table
     # (assumes niceday user id is unique and immutable)
-    existing_users = session.query(Users).filter(Users.nicedayuid == userid).count()
+    existing_users = (session.query(Users).
+                      filter(Users.nicedayuid == userid).
+                      count())
     if existing_users != 0:
         sys.exit(f'User {userid} already exists in the database.')
 
     # Add new user to the Users table
-    new_user = Users (
-        nicedayuid = userid,
-        firstname = profile['firstName'],
-        lastname = profile['lastName'],
-        location = profile['location'],
-        gender = profile['gender'],
-        dob = profile['birthDate']
+    new_user = Users(
+        nicedayuid=userid,
+        firstname=profile['firstName'],
+        lastname=profile['lastName'],
+        location=profile['location'],
+        gender=profile['gender'],
+        dob=profile['birthDate']
     )
 
     session.add(new_user)
@@ -48,6 +51,7 @@ def main(args=None):
     parser.add_argument("userid", type=int, help="User ID on Sensehealth server")
     args = parser.parse_args()
     onboard_user(args.userid)
+
 
 if __name__ == "__main__":
     main()

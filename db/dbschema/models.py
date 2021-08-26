b11c2deb4319b20825f0ddaaaccda00eff69537d
Robin Richardson <raarichardson@gmail.com>
2021-08-26 11:48:29 +0200

    Fix isort

--- db/dbschema/models.py
+++ db/dbschema/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()

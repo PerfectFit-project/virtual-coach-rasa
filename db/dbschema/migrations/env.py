55760f084f096f45fb0143006dce1edc5aaee1af
Cunliang Geng <c.geng@esciencecenter.nl>
2021-08-26 16:55:40 +0200

    Sort imports

--- db/dbschema/migrations/env.py
+++ db/dbschema/migrations/env.py
@@ -2,9 +2,9 @@ import os
 import sys
 from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from alembic import context
 from models import Base
 
 # this is the Alembic Config object, which provides
--- sensor_api/tests/test_endpoints.py
+++ sensor_api/tests/test_endpoints.py
@@ -1,5 +1,4 @@
 import pytest
-
 from sensor_api.sensorapi.run import create_app
 
 

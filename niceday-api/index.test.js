f4a59fbeffe5a411502aaa7c49c21c06b89e377d
Robin Richardson <raarichardson@gmail.com>
2021-09-02 18:58:57 +0200

    First test working with mocked server and checking response body

--- niceday-api/index.test.js
+++ niceday-api/index.test.js
@@ -4,15 +4,39 @@ const {create_niceday_api_server} = require('./index.js');
 
 const NICEDAY_TEST_SERVERPORT = 8080;
 const NICEDAY_TEST_USER_ID = 38527;
+const MOCK_USER_DATA = { id: NICEDAY_TEST_USER_ID, userProfile: {'firstName': 'Mr Mock'}};
+
 
 describe('Tests on niceday-api server using mocked goalie-js', () => {
+
   beforeAll(() => {
-    jest.mock('@sense-os/goalie-js');
-    jest.setTimeout(30000);
+
+
+    // Mock the constructor of the SenseNetwork submodule of goaliejs to return
+    // a mock with the getContact() method, which itself returns a promise containing
+    // the MOCK_USER_DATA. SenseServer.Alpha needs to be mocked too since it is used
+    // in calls to the constructor.
+    //
+    // More comprehensive documentation can be found here:
+    // https://en.wikipedia.org/wiki/Lovecraftian_horror
+    jest.mock('@sense-os/goalie-js', () => ({
+      SenseServer: () => ({
+        Alpha: undefined
+      }),
+      SenseNetwork: jest.fn().mockImplementation(() => {
+        return { getContact: (token, body) => {
+            return new Promise ((resolve, reject) => {
+              resolve(MOCK_USER_DATA);
+            });
+          }
+        };
+      })
+    }));
+
+
   });
 
   beforeEach((done) => {
-    jest.mock('@sense-os/goalie-js');
     server = create_niceday_api_server();
     server.listen(NICEDAY_TEST_SERVERPORT, () => {
       console.debug('Test server up and listening on port %d', NICEDAY_TEST_SERVERPORT);
@@ -26,9 +50,11 @@ describe('Tests on niceday-api server using mocked goalie-js', () => {
   });
 
   it('Test fetching user data from userdata/ endpoint', () => {
+
     return fetch('http://localhost:' + NICEDAY_TEST_SERVERPORT + '/userdata/' + NICEDAY_TEST_USER_ID)
-    .then(function(response) {
-        console.log(response);
-    });
+      .then(response => response.json())
+      .then(response_body =>{
+        expect(response_body).toEqual(MOCK_USER_DATA);
+      });
   });
 });

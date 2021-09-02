9493212ab2052e54b1b71f5711eaf02e8d1f2802
Robin Richardson <raarichardson@gmail.com>
2021-09-02 14:47:50 +0200

    Mock the goalie-js module before running any tests

--- niceday-api/index.test.js
+++ niceday-api/index.test.js
@@ -2,13 +2,19 @@ require('isomorphic-fetch');
 const http = require('http');
 const {create_niceday_api_server} = require('./index.js');
 
-const SERVERPORT = 8080;
+const NICEDAY_TEST_SERVERPORT = 8080;
+const NICEDAY_TEST_USER_ID = 38527;
 
 describe('Test fetching of user data', () => {
+  beforeAll(() => {
+    jest.mock('@sense-os/goalie-js');
+  });
+
   beforeEach((done) => {
+    jest.mock('@sense-os/goalie-js');
     server = create_niceday_api_server();
-    server.listen(SERVERPORT, () => {
-      console.debug('Test server up and listening on port %d', SERVERPORT);
+    server.listen(NICEDAY_TEST_SERVERPORT, () => {
+      console.debug('Test server up and listening on port %d', NICEDAY_TEST_SERVERPORT);
       done();
     });
   });
@@ -19,9 +25,9 @@ describe('Test fetching of user data', () => {
   });
 
   it('Mock Test Case', () => {
-//    return fetch('http://localhost:8080/userdata/5218')
-//    .then(function(response) {
-//        console.log(response);
-//    });
+    return fetch('http://localhost:8080/userdata/' + NICEDAY_TEST_USER_ID)
+    .then(function(response) {
+        console.log(response);
+    });
   });
 });

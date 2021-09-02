7621a9d4cf5c2c96a148205ae2ff8f5187af586d
Robin Richardson <raarichardson@gmail.com>
2021-09-02 13:21:53 +0200

    Setup and teardown of server in niceday api tests working

--- niceday-api/index.test.js
+++ niceday-api/index.test.js
@@ -1,5 +1,27 @@
-describe('Temporary Mock Test Suite', () => {
+require('isomorphic-fetch');
+const http = require('http');
+const {create_niceday_api_server} = require('./index.js');
+
+const SERVERPORT = 8080;
+
+describe('Test fetching of user data', () => {
+  beforeEach((done) => {
+    server = create_niceday_api_server();
+    server.listen(SERVERPORT, () => {
+      console.debug('Test server up and listening on port %d', SERVERPORT);
+      done();
+    });
+  });
+
+  afterEach((done) => {
+   console.debug('Stopping server');
+   server.close(done);
+  });
+
   it('Mock Test Case', () => {
-    expect(true).toEqual(true);
+//    return fetch('http://localhost:8080/userdata/5218')
+//    .then(function(response) {
+//        console.log(response);
+//    });
   });
 });

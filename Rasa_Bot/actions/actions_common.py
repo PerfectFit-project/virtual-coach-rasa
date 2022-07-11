dc789fd23abf8e171845495b6a9375fa307ea530
Walter Baccinelli <w.baccinelli@esciencecenter.nl>
2022-07-11 11:52:15 +0200

    cleaning end dialogs

---
+++ Rasa_Bot/actions/actions_common.py
@@ -0,0 +1,15 @@
+from rasa_sdk import Action
+from rasa_sdk.events import FollowupAction
+
+
+class ActionEndDialog(Action):
+    """Action to cleanly terminate the dialog."""
+    # ATM this action just call the default restart action
+    # but this can be used to perform actions that might be needed
+    # at the end of each dialog
+    def name(self):
+        return "action_end_dialog"
+
+    async def run(self, dispatcher, tracker, domain):
+
+        return [FollowupAction('action_restart')]

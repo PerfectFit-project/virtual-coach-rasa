baffd301c2ba9a57cdcfae5eaecb8961f87e36d0
Bendik Christensen <christensenbendik@gmail.com>
2022-09-07 09:28:57 +0200

    Added user preferences dummy conversation as a form. Still unsure as to whether or not the actions part works as I need to find a way to run the actions separately while doing rasa shell, but the form is triggered so that is some progress.

--- Rasa_Bot/actions/actions_preparation_dialogs.py
+++ Rasa_Bot/actions/actions_preparation_dialogs.py
@@ -1,11 +1,17 @@
 """
 Contains custom actions related to the preparation dialogs
 """
+from typing import Text, List, Any, Dict
+from datetime import datetime
 
-from rasa_sdk import Action
-from rasa_sdk.events import SlotSet
+from rasa_sdk import Tracker, FormValidationAction, Action
+from rasa_sdk.events import EventType, SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
 from virtual_coach_db.helper.definitions import PreparationInterventionComponents
 
+YES_OR_NO = ["yes", "no"]
+ALLOWED_WEEK_DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
 
 ### Slot-setting methods called for rasa to store current intervention component
 class SetSlotProfileCreation(Action):
@@ -60,3 +66,62 @@ class SetSlotGoalSetting(Action):
     async def run(self, dispatcher, tracker, domain):
         return [SlotSet("current_intervention_component",
                         PreparationInterventionComponents.GOAL_SETTING)]
+
+class ValidateUserPreferencesForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_user_preferences_form"
+
+    def validate_recursive_reminder(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate recursive_reminder` value."""
+
+        if slot_value.lower() not in YES_OR_NO:
+            dispatcher.utter_message(text=f"We only accept 'yes' or 'no' as answers")
+            return {"recursive_reminder": None}
+        dispatcher.utter_message(text=f"OK! You have answered {slot_value}.")
+        return {"recursive_reminder": slot_value}
+
+    def validate_week_days(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `week_days` value."""
+
+        if slot_value not in ALLOWED_WEEK_DAYS:
+            dispatcher.utter_message(text=f"I don't recognize that day of the week, try again!")
+            return {"week_days": None}
+        dispatcher.utter_message(text=f"OK! You want to receive reminders on {slot_value}s.")
+        return {"week_days": slot_value}
+
+    def validate_time_stamp(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `week_days` value."""
+
+        timestring = slot_value
+        format = "%H:%M:%S"
+        res = True
+
+        # using try-except to check for truth value
+        try:
+            res = bool(datetime.strptime(timestring, format))
+        except ValueError:
+            res = False
+
+        if res:
+            dispatcher.utter_message(text=f"Please submit an answer as given by the example: 20:34:20")
+            return {"time_stamp": None}
+        dispatcher.utter_message(text=f"OK! You want to receive reminders at {slot_value}.")
+        return {"time_stamp": slot_value}

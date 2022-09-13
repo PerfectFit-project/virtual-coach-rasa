b083d457e33b2a1ec2ba8a2d70e98f6d09bb413e
Bendik Christensen <christensenbendik@gmail.com>
2022-09-13 07:49:06 +0200

    Functional DB Insertion
    Can now insert user preferences into DB, to test this you need to configure your test user id in the perfectfit main .env file to be your own id

--- Rasa_Bot/actions/actions_preparation_dialogs.py
+++ Rasa_Bot/actions/actions_preparation_dialogs.py
@@ -12,7 +12,7 @@ from rasa_sdk.types import DomainDict
 from virtual_coach_db.helper.definitions import PreparationInterventionComponents
 
 from virtual_coach_db.helper.helper_functions import get_db_session
-from .helper import (store_user_preferences_to_db, get_intervention_component_id)
+from .helper import (store_user_preferences_to_db, get_intervention_component_id, week_day_to_numerical_form)
 from virtual_coach_db.dbschema.models import (Users, DialogAnswers, UserInterventionState,
                                               InterventionComponents)
 
@@ -100,11 +100,19 @@ class ValidateUserPreferencesForm(FormValidationAction):
         domain: DomainDict,
     ) -> Dict[Text, Any]:
         """Validate `week_days` value."""
-        logging.info("week days 1")
-        if slot_value not in ALLOWED_WEEK_DAYS:
-            dispatcher.utter_message(text=f"I don't recognize that day of the week, try again!")
+
+        week_days_string = slot_value
+        week_days_list = week_days_string.split(", ")
+        invalidinput = False
+
+        for weekday in week_days_list:
+            if weekday.lower() not in ALLOWED_WEEK_DAYS:
+                invalidinput = True
+
+        if invalidinput:
+            dispatcher.utter_message(text=f"Please submit the days of the week as a comma separated list!")
             return {"week_days": None}
-        dispatcher.utter_message(text=f"OK! You want to receive reminders on {slot_value}s.")
+        dispatcher.utter_message(text=f"OK! You want to receive reminders on these days of the week: {slot_value}.")
         return {"week_days": slot_value}
 
     def validate_time_stamp(
@@ -143,29 +151,38 @@ class StoreUserPreferencesToDb(Action):
 
     async def run(self, dispatcher, tracker, domain):
         logging.info("running custom action StoreUserPreferences to DB")
-        ##user_id = tracker.current_state()['sender_id']
-        user_id = 41482
+        user_id = tracker.current_state()['sender_id']
 
         logging.info("user id is: " + str(user_id))
 
         recursive = tracker.get_slot("recursive_reminder")
         week_days = tracker.get_slot("week_days")
         preferred_time_string = tracker.get_slot("time_stamp")
-        logging.info("recursive slot is: " + str(recursive) + ", week days slot is: " + str(week_days) + ", preferred time is: " + str(preferred_time))
+        logging.info("recursive slot is: " + str(recursive) + ", week days slot is: " + str(week_days) + ", preferred time is: " + str(preferred_time_string))
 
         recursive_bool = False
-        if recursive == "yes":
+        if recursive == "yes" or recursive == "Yes":
             recursive_bool = True
 
         logging.info("boolean is now: " + str(recursive_bool))
 
+        week_days_numbers = ""
+        week_days_list = week_days.split(", ")
+        for weekday in week_days_list:
+            week_days_numbers += str(week_day_to_numerical_form(weekday))
+            week_days_numbers += ","
+            logging.info(week_days_numbers)
+
+        ##TODO Set the slot in rasa
+        # When calling this in the right context, the intervention component slot should have a value.
+        # Uncomment the next two lines and remove the one under those two to switch from a hardcoded intervention component to the one decided by the slot.
         ##intervention_component_string = tracker.get_slot("current_intervention_component")
+        ##intervention_component = get_intervention_component_id(intervention_component_string)
         intervention_component = get_intervention_component_id("profile_creation")
         logging.info("storing into db, intervention comp id is: " + str(intervention_component))
 
         datetime_format = datetime.strptime(preferred_time_string, '%H:%M:%S')
-        preferred_time = datetime_format.timestamp()
         logging.info("successfully converted to timestamp, calling helper function")
 
-        store_user_preferences_to_db(user_id, intervention_component, recursive_bool, week_days, preferred_time)
+        store_user_preferences_to_db(user_id, intervention_component, recursive_bool, week_days_numbers.rstrip(week_days_numbers[-1]), datetime_format)
         return
--- Rasa_Bot/actions/helper.py
+++ Rasa_Bot/actions/helper.py
@@ -11,19 +11,27 @@ from virtual_coach_db.helper.helper_functions import get_db_session
 
 def store_dialog_answer_to_db(user_id, answer, question: DialogQuestions):
     session = get_db_session(db_url=DATABASE_URL)  # Create session object to connect db
+    logging.info("got db session")
     selected = session.query(Users).filter_by(nicedayuid=user_id).one()
+    logging.info("found user")
 
     entry = DialogAnswers(answer=answer,
                           question_id=question.value,
                           datetime=datetime.datetime.now().astimezone(TIMEZONE))
-
+    logging.info("dialog answer stored to db")
     selected.dialog_answers.append(entry)
     session.commit()  # Update database
 
 def store_user_preferences_to_db(user_id, intervention_component, recursive, week_days, preferred_time):
+    logging.info("helper method started")
     session = get_db_session(db_url=DATABASE_URL)  # Create session object to connect db
+    logging.info("session acquired")
+
     selected = session.query(Users).filter_by(nicedayuid=user_id).one()
+
+
     logging.info("established connection with DB")
+
     entry = UserPreferences(users_nicedayuid=user_id,
                             intervention_component_id=intervention_component,
                             recursive=recursive,
@@ -55,3 +63,23 @@ def get_intervention_component_id(intervention_component_name: str) -> int:
 
     intervention_component_id = selected[0].intervention_component_id
     return intervention_component_id
+
+def week_day_to_numerical_form(week_day):
+    logging.info(week_day.lower())
+    if week_day.lower() == "monday":
+        logging.info("did it")
+        return 1
+    elif week_day.lower() == "tuesday":
+        return 2
+    elif week_day.lower() == "wednesday":
+        return 3
+    elif week_day.lower() == "thursday":
+        return 4
+    elif week_day.lower() == "friday":
+        return 5
+    elif week_day.lower() == "saturday":
+        return 6
+    elif week_day.lower() == "sunday":
+        return 7
+    else:
+        return -1

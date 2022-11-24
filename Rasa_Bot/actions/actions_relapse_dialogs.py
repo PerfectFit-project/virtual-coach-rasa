20cbeb2a4d08ca1983d695546a8c5cc7f24eccb9
Walter Baccinelli <w.baccinelli@esciencecenter.nl>
2022-11-24 09:49:36 +0100

    generalized validation methods

--- Rasa_Bot/actions/actions_relapse_dialogs.py
+++ Rasa_Bot/actions/actions_relapse_dialogs.py
@@ -3,6 +3,7 @@ Contains custom actions related to the relapse dialogs
 """
 import string
 import requests
+import validator
 from .helper import get_latest_bot_utterance
 import logging
 from typing import Any, Dict, Text
@@ -16,26 +17,6 @@ from virtual_coach_db.helper.definitions import ExecutionInterventionComponents
 from .definitions import REDIS_URL
 
 celery = Celery(broker=REDIS_URL)
-
-
-def validate_number_in_range_response(n_min: int, n_max: int, response: str) -> bool:
-    try:
-        value = int(response)
-    except ValueError:
-        return False
-    if (value < 1) or (value > 2):
-        return False
-    return True
-
-
-def validate_long_enough_response(response):
-    if response is None:
-        return False
-    return len(simple_sanitize_input(response).split()) > 5
-
-
-def simple_sanitize_input(value):
-    return value.translate({c: "" for c in string.punctuation})
 
 
 # Trigger relapse phase through celery
@@ -55,6 +36,20 @@ class TriggerRelapseDialog(Action):
         return []
 
 
+class StoreHrsSituation(Action):
+    def name(self):
+        return "store_hrs_situation"
+
+    async def run(self, dispatcher, tracker, domain):
+        user_id = int(tracker.current_state()['sender_id'])  # retrieve userID
+        # get the user choice
+        choice = tracker.get_slot('hrs_situation')
+
+        # TODO: save on DB
+
+        return []
+
+
 class ValidateSmokeOrPaForm(FormValidationAction):
     def name(self) -> Text:
         return 'validate_smoke_or_pa_form'
@@ -70,22 +65,12 @@ class ValidateSmokeOrPaForm(FormValidationAction):
             return {"smoke_or_pa": None}
 
         logging.info("Performing the action to validate smoke_or_pa_form")  # Debug message
-        if not self._is_valid_input(value):
+        if not validator.validate_number_in_range_response(1, 2, value):
             dispatcher.utter_message(response="utter_did_not_understand")
             dispatcher.utter_message(response="utter_please_answer_1_2")
             return {"smoke_or_pa": None}
 
         return {"smoke_or_pa": value}
-
-    @staticmethod
-    def _is_valid_input(value):
-        try:
-            value = int(value)
-        except ValueError:
-            return False
-        if (value < 1) or (value > 2):
-            return False
-        return True
 
 
 class ValidateCraveLapseRelapseForm(FormValidationAction):
@@ -103,22 +88,12 @@ class ValidateCraveLapseRelapseForm(FormValidationAction):
         if last_utterance != 'utter_ask_crave_lapse_relapse':
             return {"crave_lapse_relapse": None}
 
-        if not self._is_valid_input(value):
+        if not validator.validate_number_in_range_response(1, 3, value):
             dispatcher.utter_message(response="utter_did_not_understand")
             dispatcher.utter_message(response="utter_please_answer_1_2_3")
             return {"crave_lapse_relapse": None}
 
         return {"crave_lapse_relapse": value}
-
-    @staticmethod
-    def _is_valid_input(value):
-        try:
-            value = int(value)
-        except ValueError:
-            return False
-        if (value < 1) or (value > 3):
-            return False
-        return True
 
 
 class ValidateEhboMeSelfForm(FormValidationAction):
@@ -137,22 +112,12 @@ class ValidateEhboMeSelfForm(FormValidationAction):
         if last_utterance != 'utter_ask_ehbo_me_self':
             return {"ehbo_me_self": None}
 
-        if not self._is_valid_input(value):
+        if not validator.validate_number_in_range_response(1, 3, value):
             dispatcher.utter_message(response="utter_did_not_understand")
             dispatcher.utter_message(response="utter_please_answer_1_2_3")
             return {"ehbo_me_self": None}
 
         return {"ehbo_me_self": value}
-
-    @staticmethod
-    def _is_valid_input(value):
-        try:
-            value = int(value)
-        except ValueError:
-            return False
-        if (value < 1) or (value > 3):
-            return False
-        return True
 
 
 class ValidateTypeAndNumberSmokeForm(FormValidationAction):
@@ -171,7 +136,7 @@ class ValidateTypeAndNumberSmokeForm(FormValidationAction):
         if last_utterance != 'utter_ask_type_smoke':
             return {"type_smoke": None}
 
-        if not self._is_valid_input_type(value):
+        if not validator.validate_number_in_range_response(1, 4, value):
             dispatcher.utter_message(response="utter_did_not_understand")
             dispatcher.utter_message(response="utter_please_answer_1_2_3_4")
             return {"type_smoke": None}
@@ -190,7 +155,7 @@ class ValidateTypeAndNumberSmokeForm(FormValidationAction):
         if last_utterance != 'utter_ask_type_smoke_confirm':
             return {"type_smoke_confirm": None}
 
-        if not self._is_valid_input_yes_no(value):
+        if not validator.validate_yes_no_answer(value):
             dispatcher.utter_message(response="utter_did_not_understand")
             dispatcher.utter_message(response="utter_please_answer_yes_no")
             return {"type_smoke_confirm": None}
@@ -212,7 +177,7 @@ class ValidateTypeAndNumberSmokeForm(FormValidationAction):
         if last_utterance != 'utter_ask_number_smoke':
             return {"number_smoke": None}
 
-        if not self._is_valid_input_number(value):
+        if not validator.validate_int_type(value):
             dispatcher.utter_message(response="utter_did_not_understand")
             dispatcher.utter_message(response="utter_please_answer_number")
             return {"number_smoke": None}
@@ -231,7 +196,7 @@ class ValidateTypeAndNumberSmokeForm(FormValidationAction):
         if last_utterance != 'utter_ask_number_smoke_confirm':
             return {"number_smoke_confirm": None}
 
-        if not self._is_valid_input_yes_no(value):
+        if not validator.validate_yes_no_answer(value):
             dispatcher.utter_message(response="utter_did_not_understand")
             dispatcher.utter_message(response="utter_please_answer_yes_no")
             return {"number_smoke_confirm": None}
@@ -240,31 +205,6 @@ class ValidateTypeAndNumberSmokeForm(FormValidationAction):
             return {"number_smoke": None, "number_smoke_confirm": None}
         else:
             return {"number_smoke_confirm": value}
-
-    @staticmethod
-    def _is_valid_input_number(value):
-        try:
-            value = int(value)
-        except ValueError:
-            return False
-        return True
-
-    @staticmethod
-    def _is_valid_input_type(value):
-        try:
-            value = int(value)
-        except ValueError:
-            return False
-        if (value < 1) or (value > 4):
-            return False
-        return True
-
-    @staticmethod
-    def _is_valid_input_yes_no(value):
-        if value.lower() in ['Ja', 'ja', 'ja.', 'Nee', 'nee', "nee."]:
-            return True
-        else:
-            return False
 
 
 class ValidateWhatDoingHowFeelSmokeForm(FormValidationAction):
@@ -297,33 +237,20 @@ class ValidateWhatDoingHowFeelSmokeForm(FormValidationAction):
             tracker: Tracker, domain: Dict[Text, Any]) -> Dict[Text, Any]:
         # pylint: disable=unused-argument
         """Validate type of smoke input confirmation"""
-        max_val = 5
         logging.info("Validate how feel smoke")  # Debug message
 
         last_utterance = get_latest_bot_utterance(tracker.events)
         if last_utterance != 'utter_ask_how_feel_smoke':
             return {"how_feel_smoke": None}
 
-        value = self._input_to_list(value, max_val)
+        valid = validator.validate_list(value, 0, 5)
 
-        if value is False:
+        if not valid:
             dispatcher.utter_message(response="utter_did_not_understand")
             dispatcher.utter_message(response="utter_please_answer_numbers")
             return {"how_feel_smoke": None}
 
         return {"how_feel_smoke": value}
-
-    @staticmethod
-    def _input_to_list(value, max_val):
-        try:
-            value = list(map(int, value.split()))
-        except ValueError:
-            return False
-        else:
-            if min(value) < 0 or max(value) > max_val:
-                return False
-            else:
-                return value
 
 
 class ValidateWithWhomEventSmokeForm(FormValidationAction):
@@ -342,9 +269,9 @@ class ValidateWithWhomEventSmokeForm(FormValidationAction):
         if last_utterance != 'utter_ask_with_whom_smoke':
             return {"with_whom_smoke": None}
 
-        value = self._input_to_list(value, max_val)
+        valid = validator.validate_list(value, 0, 6)
 
-        if value is False:
+        if not valid:
             dispatcher.utter_message(response="utter_did_not_understand")
             dispatcher.utter_message(response="utter_please_answer_numbers")
             return {"with_whom_smoke": None}
@@ -364,18 +291,6 @@ class ValidateWithWhomEventSmokeForm(FormValidationAction):
 
         return {"with_whom_smoke": value}
 
-    @staticmethod
-    def _input_to_list(value, max_val):
-        try:
-            value = list(map(int, value.split()))
-        except ValueError:
-            return False
-        else:
-            if min(value) < 0 or max(value) > max_val:
-                return False
-            else:
-                return value
-
 
 class ValidateReflectBarChartForm(FormValidationAction):
     def name(self) -> Text:
@@ -392,7 +307,7 @@ class ValidateReflectBarChartForm(FormValidationAction):
         if last_utterance != 'utter_ask_reflect_bar_chart':
             return {"reflect_bar_chart": None}
 
-        long_enough_response = validate_long_enough_response(value)
+        long_enough_response = validator.validate_long_enough_response(value, 5)
         if not long_enough_response:
             dispatcher.utter_message(response="utter_please_answer_more_words")
             return {"reflect_bar_chart": None}
@@ -416,7 +331,7 @@ class ValidateTypeAndNumberSmokeRelapseForm(FormValidationAction):
         if last_utterance != 'utter_ask_type_smoke_relapse':
             return {"type_smoke_relapse": None}
 
-        if not self._is_valid_input_type(value):
+        if not validator.validate_number_in_range_response(1, 3, value):
             dispatcher.utter_message(response="utter_did_not_understand")
             dispatcher.utter_message(response="utter_please_answer_1_2_3")
             return {"type_smoke_relapse": None}
@@ -435,10 +350,10 @@ class ValidateTypeAndNumberSmokeRelapseForm(FormValidationAction):
         if last_utterance != 'utter_ask_type_smoke_relapse_confirm':
             return {"type_smoke_relapse_confirm": None}
 
-        if not self._is_valid_input_yes_no(value):
+        if not validator.validate_yes_no_answer(value):
             dispatcher.utter_message(response="utter_did_not_understand")
             dispatcher.utter_message(response="utter_please_answer_yes_no")
-            return {"type_smoke_relapse_confirm": None}
+            return {"type_smoke_relapse": None, "type_smoke_relapse_confirm": None}
 
         if value.lower() in ['Nee', 'nee', "nee."]:
             return {"type_smoke_relapse": None, "type_smoke_relapse_confirm": None}
@@ -457,7 +372,7 @@ class ValidateTypeAndNumberSmokeRelapseForm(FormValidationAction):
         if last_utterance != 'utter_ask_number_smoke_relapse':
             return {"number_smoke_relapse": None}
 
-        if not self._is_valid_input_number(value):
+        if not validator.validate_int_type(value):
             dispatcher.utter_message(response="utter_did_not_understand")
             dispatcher.utter_message(response="utter_please_answer_number")
             return {"number_smoke_relapse": None}
@@ -476,7 +391,7 @@ class ValidateTypeAndNumberSmokeRelapseForm(FormValidationAction):
         if last_utterance != 'utter_ask_number_smoke_relapse_confirm':
             return {"number_smoke_relapse_confirm": None}
 
-        if not self._is_valid_input_yes_no(value):
+        if not validator.validate_yes_no_answer(value):
             dispatcher.utter_message(response="utter_did_not_understand")
             dispatcher.utter_message(response="utter_please_answer_yes_no")
             return {"number_smoke_relapse_confirm": None}
@@ -485,31 +400,6 @@ class ValidateTypeAndNumberSmokeRelapseForm(FormValidationAction):
             return {"number_smoke_relapse": None, "number_smoke_relapse_confirm": None}
         else:
             return {"number_smoke_relapse_confirm": value}
-
-    @staticmethod
-    def _is_valid_input_number(value):
-        try:
-            value = int(value)
-        except ValueError:
-            return False
-        return True
-
-    @staticmethod
-    def _is_valid_input_type(value):
-        try:
-            value = int(value)
-        except ValueError:
-            return False
-        if (value < 1) or (value > 3):
-            return False
-        return True
-
-    @staticmethod
-    def _is_valid_input_yes_no(value):
-        if value.lower() in ['Ja', 'ja', 'ja.', 'Nee', 'nee', "nee."]:
-            return True
-        else:
-            return False
 
 
 class ValidatePaTypeTogetherWhyFailForm(FormValidationAction):
@@ -540,7 +430,7 @@ class ValidatePaTypeTogetherWhyFailForm(FormValidationAction):
         if last_utterance != 'utter_ask_pa_together':
             return {"pa_together": None}
 
-        if not self._is_valid_input(value):
+        if not validator.validate_number_in_range_response(1, 2, value):
             dispatcher.utter_message(response="utter_did_not_understand")
             dispatcher.utter_message(response="utter_please_answer_1_2")
             return {"pa_together": None}
@@ -561,34 +451,12 @@ class ValidatePaTypeTogetherWhyFailForm(FormValidationAction):
 
         value = self._input_to_list(value, max_val)
 
-        if value is False:
+        if not validator.validate_list(value, 0, max_val):
             dispatcher.utter_message(response="utter_did_not_understand")
             dispatcher.utter_message(response="utter_please_answer_numbers")
             return {"pa_why_fail": None}
 
         return {"pa_why_fail": value}
-
-    @staticmethod
-    def _input_to_list(value, max_val):
-        try:
-            value = list(map(int, value.split()))
-        except ValueError:
-            return False
-        else:
-            if min(value) < 0 or max(value) > max_val:
-                return False
-            else:
-                return value
-
-    @staticmethod
-    def _is_valid_input(value):
-        try:
-            value = int(value)
-        except ValueError:
-            return False
-        if (value < 1) or (value > 2):
-            return False
-        return True
 
 
 class ValidateHrsNewActivityForm(FormValidationAction):
@@ -605,7 +473,7 @@ class ValidateHrsNewActivityForm(FormValidationAction):
         if last_utterance != 'utter_ask_hrs_new_activity_slot':
             return {"hrs_new_activity_slot": None}
 
-        is_valid = validate_number_in_range_response(1, 2, value)
+        is_valid = validator.validate_number_in_range_response(1, 2, value)
         if not is_valid:
             dispatcher.utter_message(response="utter_please_answer_1_2")
             return {"hrs_new_activity_slot": None}
@@ -627,9 +495,31 @@ class ValidateHrsSituationForm(FormValidationAction):
         if last_utterance != 'utter_ask_hrs_situation_slot':
             return {"hrs_situation_slot": None}
 
-        is_valid = validate_number_in_range_response(1, 7, value)
+        is_valid = validator.validate_number_in_range_response(1, 7, value)
         if not is_valid:
             dispatcher.utter_message(response="utter_please_answer_1_to_7")
             return {"hrs_situation_slot": None}
 
         return {"hrs_situation_slot": value}
+
+
+class ValidateHrsFeelingForm(FormValidationAction):
+    def name(self) -> Text:
+        return 'validate_hrs_feeling_form'
+
+    def validate_hrs_feeling_slot(
+            self, value: Text, dispatcher: CollectingDispatcher,
+            tracker: Tracker, domain: Dict[Text, Any]) -> Dict[Text, Any]:
+        # pylint: disable=unused-argument
+        """Validate hrs_feeling_slot"""
+
+        last_utterance = get_latest_bot_utterance(tracker.events)
+        if last_utterance != 'utter_ask_hrs_feeling_slot':
+            return {"hrs_feeling_slot": None}
+
+        is_valid = validator.validate_number_in_range_response(1, 8, value)
+        if not is_valid:
+            dispatcher.utter_message(response="utter_please_answer_1_to_8")
+            return {"hrs_feeling_slot": None}
+
+        return {"hrs_feeling_slot": value}

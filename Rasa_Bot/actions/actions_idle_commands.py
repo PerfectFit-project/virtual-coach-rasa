d92cc2c13ab5700dc214dc538d8701b039238348
Bouke Scheltinga <b.l.scheltinga@utwente.nl>
2023-09-13 13:40:27 +0200

    Add command to see current steps and daily step goal

--- Rasa_Bot/actions/actions_idle_commands.py
+++ Rasa_Bot/actions/actions_idle_commands.py
@@ -1,10 +1,15 @@
 from celery import Celery
 from rasa_sdk import Action
-from rasa_sdk.events import FollowupAction
+from rasa_sdk.events import SlotSet, FollowupAction
+import datetime
 
-from .helper import dialog_to_be_completed,get_current_user_phase, get_dialog_completion_state
+from .helper import (dialog_to_be_completed,
+                     get_current_user_phase,
+                     get_dialog_completion_state,
+                     get_goal_setting_chosen_sport_from_db)
 from virtual_coach_db.helper.definitions import Components
 from .definitions import REDIS_URL, FsmStates
+from sensorapi.connector import get_steps_data
 
 celery = Celery(broker=REDIS_URL)
 
@@ -105,9 +110,14 @@ class ActionSelectMenu(Action):
         # is the ehbo option to be shown (the explanatory video has been shown)
         show_ehbo = get_dialog_completion_state(user_id, Components.FIRST_AID_KIT_VIDEO)
 
+        # is the doel option to be shown (the goal setting dialog has been shown)
+        if get_goal_setting_chosen_sport_from_db(user_id) is not None:
+            show_doel = True
+        else:
+            show_doel = False
+
         # the help command is shown just in the execution
         phase = get_current_user_phase(user_id)
-
         if phase != FsmStates.EXECUTION_RUN:
             show_help = False
         else:
@@ -124,11 +134,13 @@ class ActionSelectMenu(Action):
         # show the ehbo command
         if show_ehbo:
             dispatcher.utter_message(response="utter_central_mode_options_ehbo")
-
         # show the exercise command
         dispatcher.utter_message(response="utter_central_mode_options_oefening")
         # show the medication video command
         dispatcher.utter_message(response="utter_central_mode_options_medicatie")
+        # show the doel command
+        if show_doel:
+            dispatcher.utter_message(response="utter_central_mode_options_doel")
         # show the last general statement
         dispatcher.utter_message(response="utter_central_mode_options_outro")
 
@@ -148,3 +160,22 @@ class ActionTriggerUncompletedDialog(Action):
                              (user_id, Components.CONTINUE_UNCOMPLETED_DIALOG))
         else:
             return [FollowupAction('utter_no_valid_uncompleted_dialog')]
+
+
+class ActionIdleCommandsTodaySteps(Action):
+    def name(self):
+        return "action_idle_commands_today_steps"
+
+    async def run(self, dispatcher, tracker, domain):
+        # get smoking status with 1: not (re)lapsed and 2: did (re)lapse last 4 weeks
+
+        user_id = int(tracker.current_state()['sender_id'])
+
+        start = datetime.datetime.now()
+        end = end_time + datetime.timedelta(days=1)
+
+        # get number of steps from today
+        steps_data = get_steps_data(user_id=user_id, start_date=start, end_date=end)
+        today_steps = steps_data[0]['steps']
+
+        return [SlotSet('idle_commands_today_steps', today_steps)]
